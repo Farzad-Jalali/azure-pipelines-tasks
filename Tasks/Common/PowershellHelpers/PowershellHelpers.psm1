ad101208f0e44269e8330f127ba97d443316091c
Farzad Jalali <farzad.jalali@argentgroup.com>
2020-02-27 11:07:36 +0000

    tidy up

--- Tasks/Common/PowershellHelpers/PowershellHelpers.psm1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param()
-Import-VstsLocStrings "$PSScriptRoot\module.json"
-
-# Dot-source all script files
-. $PSScriptRoot\Helpers.ps1
-
-Export-ModuleMember -Function @("Invoke-ActionWithRetries", "Get-TempDirectoryPath")
